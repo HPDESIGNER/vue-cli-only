4a5af1e449d6fc0c8f23885eda8d86b6ac14692f
eugen <ed@ipunkt.biz>
2019-07-01 14:36:27 +0200

    files added

---
+++ README.md
@@ -0,0 +1,11 @@
+# Project setup
+npm install     
+
+# Compiled for development / production / test
+npm run serve   
+npm run build   
+npm run test    
+
+
+# Lints and fixes files     
+npm run lint
---
+++ src/components/main-component.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="hello world"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center;
+}
+</style>
